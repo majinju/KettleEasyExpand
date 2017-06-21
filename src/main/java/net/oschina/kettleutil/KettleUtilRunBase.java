02e63152c7a200d962a8ea664e5cc849bb63fbe0
马进举 <jinjuma@yeah.net>
2017-06-21 23:58:13 +0800

    修改部分常量位置

---
+++ src/main/java/net/oschina/kettleutil/KettleUtilRunBase.java
@@ -0,0 +1,21 @@
+/**
+* Project Name:KettleUtil
+* Date:2016年7月6日
+* Copyright (c) 2016, jingma All Rights Reserved.
+*/
+
+package net.oschina.kettleutil;
+
+
+/**
+ * kettleUtil插件扩展基础类 <br/>
+ * 要运用于该插件的类必须继承这个类<br/>
+ * date: 2016年7月6日 <br/>
+ * @deprecated 修改域名了
+ * @see cn.benma666.kettleutil.KettleUtilRunBase
+ * @author jingma
+ * @version 
+ */
+public abstract class KettleUtilRunBase extends cn.benma666.kettleutil.KettleUtilRunBase{
+    
+}
